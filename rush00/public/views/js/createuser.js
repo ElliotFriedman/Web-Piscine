eded1f8a95d0e06babfcc008fe66951045374c67
Elliot Friedman <elliotfriedman3@gmail.com>
2019-01-13 21:48:39 -0800

    rush00 added

--- rush00/public/views/js/createuser.js
+++ rush00/public/views/js/createuser.js
@@ -1,1 +1,24 @@
-alert("Created new user");
+
+function createuser() {
+
+  var passA = document.getElementById("password").value;
+  var passB = document.getElementById("re-password").value;
+
+  if (passA === passB)
+    console.log("Passwords are equal");
+/*
+  const xhr = new XMLHttpRequest();
+  xhr.onload = function (){
+    const serverResponse = document.getElementById("submit");
+    serverResponse.innerHTML = this.responseText;
+  }
+
+  xhr.open("POST", "server.js");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  var toSend = "pass=" + passA + "passb=" + passB;
+  xhr.send(toSend, filename);
+  */
+  console.log("pass a: " + passA + "\npass b: " + passB);
+
+}
